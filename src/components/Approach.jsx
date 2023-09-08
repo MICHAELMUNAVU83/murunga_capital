ce1274bd0804ae14dddc2f75857f4ebd81fca94d
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-09-08 09:54:01 +0300

    added fonts

--- src/components/Approach.jsx
+++ src/components/Approach.jsx
@@ -3,19 +3,18 @@ import React from "react";
 const Approach = () => {
   return (
     <div className="py-8 w-[90%] mx-auto">
-      <div className="flex flex-col gap-0">
-        <p className="w-[100%] bg-black h-[1px] " />
-        <p>Our Approach</p>
+      <div className="w-[100%] bourtontitle border-t-[5px] border-[#27419B]  text-4xl bg-clip-text text-transparent bg-gradient-to-r from-[#27419B] to-[#21B4EE]">
+        MURUNGA CAPITAL GROUP LLC
       </div>
 
-      <div className="w-[100%] flex justify-between ">
+      <div className="w-[100%] mt-4 flex justify-between ">
         <div className="w-[45%] flex flex-col gap-4">
-          <p>
+          <p className="text-4xl bourtontitle my-4">
             Economic Development through a 360 lens of Charity, Programs and
             Entrepreneurship
           </p>
           <div className="w-[100%] flex justify-between">
-            <div className="w-[85%] border-t-2 border-black">
+            <div className="w-[85%] text-xl bourtontitle border-t-2 border-black">
               MURUNGA CAPITAL GROUP LLC
             </div>
             <div className="w-[15%] ">
@@ -25,21 +24,27 @@ const Approach = () => {
         </div>
 
         <p className="h-[400px] w-[1px] bg-black"></p>
-        <div className="w-[45%] flex flex-col gap-4">
+        <div className="w-[45%] p-4 text-[18px] flex flex-col trebuc justify-between ">
           <p>
             Institutional investors, emerging market entrepreneurs, and
             multinationals, are often skeptical about seeking new ventures in
-            lesser-known markets. MURUNGA CAPITAL GROUP, LLC combines the
-            diverse perspectives of macroeconomic, finance, strategy, and legal
-            experts to reduce the perceived risks of doing business in Africa.
-            As seasoned global experts, we leverage the human and financial
-            capital to fill the various institutional voids to drive growth in
-            the region. We succeed because we look at Economic Development
-            through a 360 lens of Charity, Programs and Entrepreneurship.
+            lesser-known markets.
+          </p>
+
+          <p>
+            MURUNGA CAPITAL GROUP, LLC combines the diverse perspectives of
+            macroeconomic, finance, strategy, and legal experts to reduce the
+            perceived risks of doing business in Africa. As seasoned global
+            experts, we leverage the human and financial capital to fill the
+            various institutional voids to drive growth in the region.
+          </p>
+          <p>
+            We succeed because we look at Economic Development through a 360
+            lens of Charity, Programs and Entrepreneurship.
           </p>
         </div>
       </div>
-      <div className="flex justify-center mt-4 p-4 items-center bg-blue-500 w-[100%]">
+      <div className="flex justify-center bg-gradient-to-r rounded-[12px] hover:scale-105 transition-all ease-in-out duration-500 cursor-pointer text-2xl text-white  from-[#27419B] to-[#21B4EE] bourtontitle uppercase mt-4 p-4 items-center bg-blue-500 w-[100%]">
         request for client testimonials
       </div>
     </div>
